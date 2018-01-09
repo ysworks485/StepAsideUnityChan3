b352c0e8198db71c0020c913542864f0d751b135
LAPTOP-8GD7ET5M\ym485 <ym485r@yahoo.co.jp>
2018-01-10 00:14:00 +0900

    Lesson6_課題・発展課題
    課題：DestroyObjectクラスを新規作成して(各オブジェクトにアタッチ)画面外に出たオブジェクトを削除。　発展課題：itemGeneratorクラスを変更してUnityちゃんの位置を判断して動的に道路上にオブジェクトを生成。

---
+++ Assets/DestroyObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObject : MonoBehaviour {
+
+    ////////////////////////////////////////////////////////////////////////////////////
+    // Lesson6 課題作成（画面外に出たオブジェクトを削除する）
+    ////////////////////////////////////////////////////////////////////////////////////
+    public void OnBecameInvisible()
+    {
+        Destroy(this.gameObject);
+    }
+
+}
